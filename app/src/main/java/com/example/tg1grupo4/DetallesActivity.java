bc9921fb26e01f713ea9b95c2e619fc51a5b4353
Jose Miguel <josemimarcocapdevila@gmail.com>
2022-11-27 13:38:24 +0100

    vincular detalles activity con lista trabajos

--- app/src/main/java/com/example/tg1grupo4/DetallesActivity.java
+++ app/src/main/java/com/example/tg1grupo4/DetallesActivity.java
@@ -12,31 +12,38 @@ import android.widget.TextView;
 
 public class DetallesActivity extends AppCompatActivity {
 
-    TextView etiqueta;
-    EditText campo1,campo2;
-    Spinner comboAlumno;
-    Button buttonBorrar;
+    private Trabajos Item;
+    private TextView textViewDetallesTarea;
+    private EditText EditTextNota,EditTextComentario;
+    private Spinner spinner_alumnos;
+    private Button buttonBorrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalles);
 
-        etiqueta = (TextView) findViewById(R.id.textViewDetallesTarea);
-        campo1 = (EditText) findViewById(R.id.EditTextNota);
-        campo2 = (EditText) findViewById(R.id.EditTextComentario);
-        comboAlumno = (Spinner) findViewById(R.id.spinner_alumnos);
+        Item = (Trabajos) getIntent().getSerializableExtra("objetoData");
+
+        textViewDetallesTarea = (TextView) findViewById(R.id.textViewDetallesTarea);
+        EditTextNota = (EditText) findViewById(R.id.EditTextNota);
+        EditTextComentario = (EditText) findViewById(R.id.EditTextComentario);
+        spinner_alumnos = (Spinner) findViewById(R.id.spinner_alumnos);
         buttonBorrar = (Button)findViewById(R.id.buttonBorrar);
+
+        textViewDetallesTarea.setText(Item.getTitulo());
+        EditTextNota.setText("");
+        EditTextComentario.setText("");
 
         buttonBorrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                campo1.setText("");
-                campo2.setText("");
+                EditTextNota.setText("");
+                EditTextComentario.setText("");
             }
         });
         ArrayAdapter<CharSequence>adapter=ArrayAdapter.createFromResource(this,R.array.combo_alumnos, android.R.layout.simple_spinner_item);
-        comboAlumno.setAdapter(adapter);
+        spinner_alumnos.setAdapter(adapter);
     }
 
     public void onclick(View view){
