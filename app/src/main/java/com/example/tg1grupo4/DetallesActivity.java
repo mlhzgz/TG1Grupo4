4c6c25f8b4dbc363fdb699129fcb6d4d7bd91481
unknown <mikeliza2001@outlook.es>
2022-11-29 15:58:50 +0100

    validacion campos y orden de metodos

--- app/src/main/java/com/example/tg1grupo4/DetallesActivity.java
+++ app/src/main/java/com/example/tg1grupo4/DetallesActivity.java
@@ -18,11 +18,21 @@ public class DetallesActivity extends AppCompatActivity {
     private EditText EditTextNota,EditTextComentario;
     private Spinner spinner_alumnos;
     private Button buttonBorrar;
+    private Button botonGuardar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalles);
+
+        botonGuardar=(Button) findViewById(R.id.buttonGuardar);
+        botonGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Guardar();
+
+            }
+        });
 
         Item = (Trabajos) getIntent().getSerializableExtra("objetoData");
 
@@ -39,22 +49,44 @@ public class DetallesActivity extends AppCompatActivity {
         buttonBorrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast toast1= Toast.makeText(getApplicationContext(),
-                        "Datos reinicializados con éxito", Toast.LENGTH_SHORT);
-                toast1.show();
-                EditTextNota.setText("");
-                EditTextComentario.setText("");
-
+                Borrar();
             }
         });
+
+
         ArrayAdapter<CharSequence>adapter=ArrayAdapter.createFromResource(this,R.array.combo_alumnos, android.R.layout.simple_spinner_item);
         spinner_alumnos.setAdapter(adapter);
     }
 
-    public void onclick(View view){
-        switch (view.getId()){
-            case R.id.buttonGuardar:
-                break;
-        }
+
+    public void Borrar(){
+        Toast toast1= Toast.makeText(getApplicationContext(),
+                "Datos reinicializados con éxito", Toast.LENGTH_SHORT);
+        toast1.show();
+        EditTextNota.setText("");
+        EditTextComentario.setText("");
     }
+
+    public void Guardar(){
+        Toast toast1= Toast.makeText(getApplicationContext(),
+                "Guardando...", Toast.LENGTH_SHORT);
+        toast1.show();
+
+        boolean retorno = true;
+        String cNota = EditTextNota.getText().toString();
+        String cComentario = EditTextComentario.getText().toString();
+
+        if(cNota.isEmpty()){
+            EditTextNota.setError("El campo no puede quedar vacío");
+            retorno = false;
+        }
+
+        if(cNota.isEmpty()){
+            EditTextComentario.setError("El campo no puede quedar vacío");
+            retorno = false;
+        }
+
+    }
+
+
 }
